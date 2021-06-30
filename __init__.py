00187963739134f2dca188bc80e8402f3c3b742b
SpectralVectors <robmuir1@gmail.com>
2021-06-30 17:37:56 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Mind Mapper",
     "author": "Spectral Vectors",
-    "version": (0, 8, 3),
+    "version": (0, 8, 4),
     "blender": (2, 90, 0),
     "location": "Mind Mapper - Custom Node Editor",
     "description": "A custom, node based flow chart for text",
@@ -10,4 +10,38 @@ bl_info = {
     "category": "Custom Nodes",
 }
 
-from .Mindmapper import *
+from .Mindmapper import (
+    MindmapTree,
+    MindmapNode,
+    MindMapperPreferences,
+    MindmapNodeSocket,
+    UpdateNodes,
+    )
+
+classes = (
+    MindmapTree,
+    MindmapNode,
+    MindMapperPreferences,
+    MindmapNodeSocket,
+    UpdateNodes,
+)
+
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+    nodeitems_utils.register_node_categories('Mindmap', node_categories)
+
+
+def unregister():
+    nodeitems_utils.unregister_node_categories('Mindmap')
+
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+
+if __name__ == "__package__":
+    register()

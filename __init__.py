2e5dfe898f7247888b3510eac6baa3fc4a00d41b
SpectralVectors <robmuir1@gmail.com>
2021-06-30 11:45:10 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+bl_info = {
+    "name": "Mind Mapper",
+    "author": "Spectral Vectors",
+    "version": (0, 8, 3),
+    "blender": (2, 90, 0),
+    "location": "Mind Mapper - Custom Node Editor",
+    "description": "A custom, node based flow chart for text",
+    "warning": "",
+    "doc_url": "",
+    "category": "Custom Nodes",
+}
+
+from .Mindmapper import *

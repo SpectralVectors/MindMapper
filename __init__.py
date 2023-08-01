a99e8f940853bde9ec9e9756c46d9ec58eacd37c
SpectralVectors <robmuir1@gmail.com>
2023-07-31 22:28:52 -0400

    All Node Editors, Note Node, Better Update
    Big changes:
    - Introducing the Note node - a simplified Mindmap that only displays text and only has one output
    - Mindmap and Note are now available in all Node Editors - Shader, Geometry, Compositing, as well as Mindmapper
    - Categories have been renamed/reorganized: Mindmap and Note are available in the 'Extra' category in all Node Editors, Mindmapper now has a 'Layout' category for Reroute and Frame
    - the Node Update function has been rewritten: now autoupdates on changing the number of inputs/ouputs, no longer need to press the 'Update Node' button
    - Moved the props behind a Triangle dropdown on the Node itself

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 bl_info = {
     "name": "Mind Mapper",
-    "author": "Spectral Vectors",
-    "version": (0, 8, 3),
+    "author": "Spectral Vectors, tin2tin, Bazza, cannibalox",
+    "version": (0, 9, 0),
     "blender": (2, 90, 0),
-    "location": "Mind Mapper - Custom Node Editor",
-    "description": "A custom, node based flow chart for text",
+    "location": "Custom Node Editor, Shader Node Editor",
+    "description": "A multi-line text and image flow chart node",
     "warning": "",
     "doc_url": "",
-    "category": "Custom Nodes",
+    "category": "Node",
 }
 
 import bpy, textwrap
-from bpy.types import NodeTree, Node, NodeSocket, NodeReroute, NodeFrame
+from bpy.types import NodeTree, Node, NodeSocket
 
 class MindMapperPreferences(bpy.types.AddonPreferences):
     bl_idname = __name__
@@ -38,7 +38,6 @@ class MindMapperPreferences(bpy.types.AddonPreferences):
         row.prop(self, 'ShowInNode')
         row.prop(self, 'WrapAmount')
 
-
 # Mindmap-style custom Node editor
 # Derived from the NodeTree base type, similar to Menu, Operator, Panel, etc.
 class MindmapTree(NodeTree):
@@ -50,6 +49,7 @@ class MindmapTree(NodeTree):
     bl_label = "Mind Mapper"
     # Icon identifier
     bl_icon = 'NETWORK_DRIVE'
+
 
 # Custom socket type
 class MindmapNodeSocket(NodeSocket):
@@ -72,23 +72,18 @@ class MindmapNodeSocket(NodeSocket):
 class MindmapTreeNode:
     @classmethod
     def poll(cls, ntree):
-        return ntree.bl_idname == 'MindmapTreeType'
+        return ntree.bl_idname == 'MindmapTreeType' or 'ShaderNodeTree' or 'GeometryNodeTree'
 
-class UpdateNodes(bpy.types.Operator):
-    bl_idname = 'node.update_nodes'
-    bl_label = 'Update Node Sockets'
-    bl_description = 'Updates to Node to show the correct number of Input/Output sockets'
 
-    def execute(self, context):
-        #my_node = bpy.context.active_node
-        bpy.ops.node.add_node(type='MindmapNodeType')
-        bpy.ops.node.delete()
-        bpy.ops.node.add_node(type='MindmapNodeType')
-        bpy.ops.node.delete()
-        bpy.ops.node.add_node(type='MindmapNodeType')
-        bpy.ops.node.delete()
-        #bpy.context.active_node = my_node
-        return {'FINISHED'}
+def update_nodes(self, context):
+    node = context.active_node
+    node_tree = context.space_data.edit_tree
+    nodes = node_tree.nodes
+    reroute = nodes.new(type="NodeReroute")
+    links = node_tree.links
+    links.new(node.outputs[0], reroute.inputs[0])
+    node.select = False
+    bpy.ops.node.delete()
 
 
 def label_multiline(context, text, wrap_width, parent):
@@ -99,7 +94,6 @@ def label_multiline(context, text, wrap_width, parent):
     wrapper = textwrap.TextWrapper(width=chars)
     text_lines = [wrapped_line for line in text.splitlines() for wrapped_line in wrapper.wrap(text=line)]
     [parent.label(text=text_line) for text_line in text_lines]
-
 
 # Derived from the Node base type.
 class MindmapNode(Node, MindmapTreeNode):
@@ -120,8 +114,8 @@ class MindmapNode(Node, MindmapTreeNode):
     )
 
     my_title_prop: bpy.props.StringProperty(
-        name= '',
-        default= 'Mindmap'
+        name='',
+        default='Mindmap'
     )
 
     my_node_color: bpy.props.FloatVectorProperty(
@@ -143,10 +137,10 @@ class MindmapNode(Node, MindmapTreeNode):
     )
 
     node_image : bpy.props.StringProperty(
-        name = "",
-        description = "Filename of the Node's image.",
-        default = "Render Result",
-        subtype = 'FILE_NAME'
+        name="",
+        description="Filename of the Node's image.",
+        default="Render Result",
+        subtype='FILE_NAME'
     )
 
     node_inputs : bpy.props.IntProperty(
@@ -155,7 +149,7 @@ class MindmapNode(Node, MindmapTreeNode):
         default=1,
         min=1,
         max=20,
-
+        update=update_nodes,
     )
 
     node_outputs : bpy.props.IntProperty(
@@ -164,7 +158,7 @@ class MindmapNode(Node, MindmapTreeNode):
         default=1,
         min=1,
         max=20,
-
+        update=update_nodes,
     )    
 
     # === Optional Functions ===
@@ -224,19 +218,19 @@ class MindmapNode(Node, MindmapTreeNode):
         for text_line in text_lines:
             box.label(text=text_line)
 
-        layout.separator(factor=2)
-
         if addon_prefs.ShowInNode:
-            if self.show_in_single_node:
+            column = layout.column(align=True)
+            row = column.row(align=True)
+            icon = 'TRIA_DOWN' if self.show_in_single_node else 'TRIA_RIGHT'
+            row.prop(self, 'show_in_single_node', icon=icon, icon_only=True)
 
-                column = layout.column(align=True)
-                row = column.row(align=True)
+            if self.show_in_single_node:
                 row.prop(self, "my_title_prop", icon='GREASEPENCIL')
                 row.prop(self, "color", text='', icon='MATERIAL')
 
-                row = column.row(align=True)
-                row.prop_search(self, 'node_image',  bpy.data, 'images')
-                row.operator('image.open')
+                column = column.column(align=True)
+                column.prop_search(self, 'node_image',  bpy.data, 'images')
+                column.operator('image.open', text='Open Image from File')
 
                 row = column.row(align=True)
                 row.prop(self, "my_string_prop", icon='GREASEPENCIL')
@@ -244,9 +238,6 @@ class MindmapNode(Node, MindmapTreeNode):
                 row = column.row(align=True)
                 row.prop(self, "node_inputs")
                 row.prop(self, "node_outputs")
-
-                row = column.row(align=True)
-                row.operator('node.update_nodes')
 
 
     # Detail buttons in the sidebar.
@@ -272,13 +263,8 @@ class MindmapNode(Node, MindmapTreeNode):
         row.prop(self, "node_inputs")
         row.prop(self, "node_outputs")
 
-        row = column.row()
-        row.operator('node.update_nodes')
-
         layout.prop(self, "show_in_single_node")
 
-        #for i in range(self.node_inputs):
-        #    layout.prop(self, "node_input_name")
 
     # Optional: custom label
     # Explicit user label overrides this, but here we can define a label dynamically
@@ -286,35 +272,145 @@ class MindmapNode(Node, MindmapTreeNode):
         return self.my_title_prop
 
     def update(self):
-
         if self.node_image:
             bpy.data.images[self.node_image].use_fake_user = True
 
-        if len(self.inputs) < self.node_inputs:
+        if not len(self.inputs) == self.node_inputs:
+            for input in self.inputs:
+                self.inputs.remove(input)
             for i in range(self.node_inputs):
-                if i > 0:
-                    self.node_input_name = str(i + 1)
-                    i = self.inputs.new('MindmapNodeSocketType', name=self.node_input_name)
-                    i.display_shape = 'SQUARE_DOT'
+                i+=1
+                self.node_input_name = str(i)
+                i = self.inputs.new('MindmapNodeSocketType', name=self.node_input_name)
+                i.display_shape = 'SQUARE_DOT'
 
-        if len(self.inputs) > self.node_inputs:
-            inputs = reversed(self.inputs)
-            for input in inputs:
-                while len(self.inputs) > self.node_inputs:
-                    self.inputs.remove(input)
-
-        if len(self.outputs) < self.node_outputs:
+        if not len(self.outputs) == self.node_outputs:
+            for output in self.outputs:
+                self.outputs.remove(output)
             for j in range(self.node_outputs):
-                if j > 0:
-                    self.node_output_name = str(j + 1)
-                    j = self.outputs.new('MindmapNodeSocketType', name=self.node_output_name)
-                    j.display_shape = 'SQUARE_DOT' 
+                j+=1
+                self.node_output_name = str(j)
+                j = self.outputs.new('MindmapNodeSocketType', name=self.node_output_name)
+                j.display_shape = 'SQUARE_DOT' 
 
-        if len(self.outputs) > self.node_outputs:
-            outputs = reversed(self.outputs)
-            for output in outputs:
-                while len(self.outputs) > self.node_outputs:
-                    self.outputs.remove(output)
+
+# Note Node
+class NoteNode(Node, MindmapTreeNode):
+    # === Basics ===
+    # Description string
+    '''A custom node'''
+    # Optional identifier string. If not explicitly defined, the python class name is used.
+    bl_idname = 'NoteNodeType'
+    # Label for nice name display
+    bl_label = "Note"
+    # Icon identifier
+    bl_icon = 'TEXT'
+    
+    # === Custom Properties ===
+    my_string_prop: bpy.props.StringProperty(
+        name= '',
+        default= 'Once upon a time, a long line of text was divided into many smaller lines, the people rejoiced, and all was well in the land of Blender...',
+    )
+
+    my_title_prop: bpy.props.StringProperty(
+        name='',
+        default='Note'
+    )
+
+    my_node_color: bpy.props.FloatVectorProperty(
+        name='',
+        default=(0.6,0.6,0),
+        subtype='COLOR',
+    )
+
+    my_text_size: bpy.props.IntProperty(
+        name='Text Size',
+        default=12,
+        soft_min=8,
+        soft_max=64,
+    )
+
+    show_in_single_node: bpy.props.BoolProperty(
+        name='Show Shortcuts in Node (single)',
+        default=True,
+    )
+    # === Optional Functions ===
+    # Initialization function, called when a new node is created.
+    def init(self, context):
+        self.use_custom_color = True
+        self.color = self.my_node_color
+        self.width = 250
+        j = self.outputs.new('MindmapNodeSocketType', name='>')
+        j.display_shape = 'SQUARE_DOT'
+        j.link_limit = 0
+
+    # Copy function to initialize a copied node from an existing one.
+    def copy(self, node):
+        print("Copying from node ", node)
+
+    # Free function to clean up on removal.
+    def free(self):
+        print("Removing node ", self, ", Goodbye!")
+
+    # Additional buttons displayed on the node.
+    def draw_buttons(self, context, layout):
+        preferences = context.preferences
+        addon_prefs = preferences.addons[__name__].preferences
+
+        # Get the width and create a text wrapper
+        characters = int(self.width / addon_prefs.WrapAmount)
+        wrapper = textwrap.TextWrapper(width=characters)
+
+        text = self.my_string_prop
+        lines = text.split("\n")
+        text_lines = []
+
+        # Wrap each line separately
+        for line in lines:
+            text_lines += wrapper.wrap(text=line)
+
+        box = layout.box()
+        box = box.column(align=True)
+
+        for text_line in text_lines:
+            box.label(text=text_line)
+
+        row = layout.row(align=True)
+        icon = 'TRIA_DOWN' if self.show_in_single_node else 'TRIA_RIGHT'
+        row.prop(self, 'show_in_single_node', icon=icon, icon_only=True)
+
+        if self.show_in_single_node:
+            row.prop(self, "my_title_prop", icon='GREASEPENCIL')
+            row.prop(self, "color", text='', icon='MATERIAL')
+
+            row = layout.row(align=True)
+            row.prop(self, "my_string_prop", icon='GREASEPENCIL')
+
+
+    # Detail buttons in the sidebar.
+    # If this function is not defined, the draw_buttons function is used instead
+    def draw_buttons_ext(self, context, layout):
+        column = layout.column()
+        row = column.row()
+        row.prop(self, "my_title_prop", icon='GREASEPENCIL')
+        
+        col = column.box().column(align=True)
+        preferences = context.preferences
+        addon_prefs = preferences.addons[__name__].preferences       
+        wrap_width = int(self.width / addon_prefs.WrapAmount)
+        text = self.my_string_prop
+        label_multiline(context, text, wrap_width, col)
+        col.prop(self, "my_string_prop", icon='GREASEPENCIL')
+
+
+    # Optional: custom label
+    # Explicit user label overrides this, but here we can define a label dynamically
+    def draw_label(self):
+        return self.my_title_prop
+
+    def update(self):
+        self.my_string_prop = self.my_string_prop
+
 
 ### Node Categories ###
 # Node categories are a python system for automatically
@@ -330,14 +426,22 @@ from nodeitems_utils import NodeCategory, NodeItem
 class MyNodeCategory(NodeCategory):
     @classmethod
     def poll(cls, context):
+        return context.space_data.tree_type == 'MindmapTreeType' or 'ShaderNodeTree' or 'GeometryNodeTree'
+
+class LayoutCategory(NodeCategory):
+    @classmethod
+    def poll(cls, context):
         return context.space_data.tree_type == 'MindmapTreeType'
 
 # all categories in a list
 node_categories = [
     # identifier, label, items list
-    MyNodeCategory('MINDMAPNODES', "Mindmapper", items=[
+    MyNodeCategory('EXTRA', "Extra", items=[
         # our basic node
         NodeItem("MindmapNodeType"),
+        NodeItem("NoteNodeType"),
+    ]),
+    LayoutCategory('LAYOUT', "Layout", items=[
         NodeItem("NodeReroute"),
         NodeItem("NodeFrame"),
     ]),
@@ -348,7 +452,7 @@ classes = (
     MindmapNode,
     MindMapperPreferences,
     MindmapNodeSocket,
-    UpdateNodes,
+    NoteNode,
 )
 
 
